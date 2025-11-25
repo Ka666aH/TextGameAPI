f850434388d842f423d22586109f2e7811b17b06
Ka666aH <dmitryhoroshev05@mail.ru>
2025-11-25 14:40:25 +0300

    Увеличить здоровье игрока.

--- GameServices.cs
+++ GameServices.cs
@@ -10,7 +10,7 @@
         public Weapon Weapon { get; set; } = Fists.DefaultFists;
         public Helm? Helm { get; set; }
         public Chestplate? Chestplate { get; set; }
-        public const int DefaultMaxHealth = 10;
+        public const int DefaultMaxHealth = 100;
         public int MaxHealth { get; set; } = DefaultMaxHealth;
         public int CurrentHealth { get; set; } = DefaultMaxHealth;
         public int Coins { get; set; }
