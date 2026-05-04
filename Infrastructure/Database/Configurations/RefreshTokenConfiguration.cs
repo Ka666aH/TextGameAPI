e1623e55d2d06fae011ad7559125366a5edd743c
Ka666aH <dmitryhoroshev05@mail.ru>
2026-05-04 14:01:38 +0300

    Добавить индекс по токену в RefreshToken. Добавить миграцию.

---
+++ Infrastructure/Database/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TextGame.Domain.Entities;
+
+namespace TextGame.Infrastructure.Database.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasIndex(x => x.Token).IsUnique();
+        }
+    }
+}
---
+++ Infrastructure/Database/Migrations/20260504105528_token-index.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextGame.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class tokenindex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens");
+        }
+    }
+}
