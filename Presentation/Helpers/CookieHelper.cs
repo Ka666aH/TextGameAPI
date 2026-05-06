bed2de33b6e4452c87b6214cf737c6d72d25d6d5
Ka666aH <dmitryhoroshev05@mail.ru>
2026-05-06 09:02:27 +0300

    Реализовать хелпер для работы с куки.

---
+++ Presentation/Helpers/CookieHelper.cs
@@ -0,0 +1,17 @@
+﻿using TextGame.Infrastructure.Token;
+
+namespace TextGame.Presentation.Helpers
+{
+    public static class CookieHelper
+    {
+        public static void SetRefreshCookie(this HttpResponse response, string value) =>
+            response.Cookies.Append(Parameters.RefreshToken, value);
+        public static void SetAccessCookie(this HttpResponse response, string value) =>
+            response.Cookies.Append(Parameters.AccessToken, value);
+        public static void DeleteAuthCookies(this HttpResponse response)
+        {
+            response.Cookies.Delete(Parameters.RefreshToken);
+            response.Cookies.Delete(Parameters.AccessToken);
+        }
+    }
+}
