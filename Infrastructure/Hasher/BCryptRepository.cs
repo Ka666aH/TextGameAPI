d3927b9e957c6339ee97e3b2ff9fb4e6f57e04a2
Ka666aH <dmitryhoroshev05@mail.ru>
2026-05-08 10:49:20 +0300

    Исправить метод верификации хэшера.

--- Infrastructure/Hasher/BCryptRepository.cs
+++ Infrastructure/Hasher/BCryptRepository.cs
@@ -8,6 +8,6 @@ namespace TextGame.Infrastructure.PasswordHasher
             BCrypt.Net.BCrypt.EnhancedHashPassword(password);
 
         public bool Verify(string password, string hashedPassword) =>
-            BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+            BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
     }
 }
