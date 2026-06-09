657426d02a0e3943e747b3d57e78651bdb31ef31
Ka666aH <dmitryhoroshev05@mail.ru>
2026-06-09 12:26:47 +0300

    Изменить время жизни кэша.

--- TextGameAPI/Infrastructure/Cache/CacheParameters.cs
+++ TextGameAPI/Infrastructure/Cache/CacheParameters.cs
@@ -2,7 +2,7 @@
 {
     public static class CacheParameters
     {
-        public static readonly TimeSpan GameSessionLifetime = TimeSpan.FromDays(7);
+        public static readonly TimeSpan GameSessionLifetime = TimeSpan.FromMinutes(10);
         public const string GameSessionStateKeyPrefix = "GameSessionState:";
     }
 }
