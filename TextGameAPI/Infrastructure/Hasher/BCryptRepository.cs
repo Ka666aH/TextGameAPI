4fba171ff832eed58169c3111202d4b8af809ef6
Ka666aH <dmitryhoroshev05@mail.ru>
2026-06-09 12:38:03 +0300

    Указать явный work factor в хэшере.

--- TextGameAPI/Infrastructure/Hasher/BCryptRepository.cs
+++ TextGameAPI/Infrastructure/Hasher/BCryptRepository.cs
@@ -5,7 +5,7 @@ namespace TextGame.Infrastructure.PasswordHasher
     public class BCryptRepository : IHasher
     {
         public string Hash(string password) =>
-            BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+            BCrypt.Net.BCrypt.EnhancedHashPassword(password, 11);
 
         public bool Verify(string password, string hashedPassword) =>
             BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
